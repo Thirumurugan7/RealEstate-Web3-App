97cdb896aadf48c79bb282e5b8e9ad5e0f5981f5
thirumurugan7 <thiru82003@gmail.com>
2022-11-09 22:39:44 +0530

    day1

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate {
-
+contract RealEstate is ERC721URIStorage {
+    //to
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 }
