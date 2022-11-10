782820e772527bbbaf533b592ad8b4440b60e804
thirumurugan7 <thiru82003@gmail.com>
2022-11-10 20:44:15 +0530

    written nft smartcontract using openzepplen

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -8,4 +8,19 @@ contract RealEstate is ERC721URIStorage {
     //to
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    constructor() ERC721("RealEstate","REAL"){}
+
+    function mint(string memory tokneURI) public returns (uint256){
+        _tokenIds.increment(); // add 1 each time
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender,newItemId);
+        _setTokenURI(newItemId,tokneURI);
+
+        return newItemId;
+    }
+    function totalSupply() public view returns (uint256){
+        return _tokenIds.current();
+    }
 }
