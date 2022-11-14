5fda05fc28f29e4f40e286576953a72f4df0e257
thirumurugan7 <thiru82003@gmail.com>
2022-11-14 23:45:18 +0530

    started frontend

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+    console.log(accounts[0]);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
-      <div className='cards__section'>
-
+      <div className="cards__section">
         <h3>Welcome to Millow</h3>
-
       </div>
-
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,30 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">BUY</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
 
-}
+      <div className="nav__brand">
+        <img src={logo} alt="logo" />
+        <h1>Millow</h1>
+      </div>
+
+      <button type="button" className="nav__connect">
+        0x0........
+      </button>
+    </nav>
+  );
+};
 
 export default Navigation;
