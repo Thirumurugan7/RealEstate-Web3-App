397c5c115b2eaab6ff63f01685c8c48789d73773
thirumurugan7 <thiru82003@gmail.com>
2022-11-15 09:25:11 +0530

    working on the listing

--- src/App.js
+++ src/App.js
@@ -14,10 +14,41 @@ import Escrow from "./abis/Escrow.json";
 import config from "./config.json";
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
   const [account, setAccount] = useState(null);
+  const [homes, setHomes] = useState([]);
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider
+    );
+    //
+    const totalSupply = await realEstate.totalSupply();
 
+    const homes = [];
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+    console.log(homes);
+
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider
+    );
+    setEscrow(escrow);
+    console.log(totalSupply);
     window.ethereum.on("accountChanged", async () => {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -34,23 +65,27 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className="cards__section">
         <h3>Homes for you</h3>
         <hr />
         <div className="cards">
-          <div className="card">
-            <div className="card__image">
-              <img src="" alt="Home" />
+          {homes.map((home, index) => (
+            <div className="card key={index}">
+              <div className="card__image">
+                <img src="" alt="Home" />
+              </div>
+              <div className="card__info">
+                <h4>1 ETH</h4>
+                <p>
+                  <strong>1</strong>bds |<strong>2</strong>ba |
+                  <strong>3</strong>
+                  sqft
+                </p>
+                <p>1234 Elm t</p>
+              </div>
             </div>
-            <div className="card__info">
-              <h4>1 ETH</h4>
-              <p>
-                <strong>1</strong>bds |<strong>2</strong>ba |<strong>3</strong>
-                sqft
-              </p>
-              <p>1234 Elm t</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
